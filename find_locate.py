327907dd951c48f1a7605dbc42a1d0e83b352f30
Ali Garivani <72452146+aligarivani@users.noreply.github.com>
2022-12-10 22:33:40 +0330

    Update find_locate.py

--- find_locate.py
+++ find_locate.py
@@ -1,15 +1,37 @@
 import requests
 import ipapi
 from colorama import Fore, init
-
+import time
+import os
+import platform
 init()
 
-ip_client = requests.get('https://api.ipify.org').text
-location_client = ipapi.location(ip_client)['country']
-city = ipapi.location(ip_client)['region']
-if location_client == 'IR':
-    print(Fore.RED+'[*] '+Fore.LIGHTCYAN_EX+'sorry you location' +
-          Fore.LIGHTRED_EX+f' {city} '+Fore.LIGHTCYAN_EX+'please use vpn '.format(city))
-else:
-    print(Fore.GREEN+'[*] '+Fore.LIGHTCYAN_EX+'so your location' +
-          Fore.LIGHTGREEN_EX+f' {city} '+Fore.LIGHTCYAN_EX+'is good you full acsess ')
+os_name = platform.uname()[0]
+
+
+def find():
+    print(Fore.LIGHTMAGENTA_EX+'Start Resived Ip')
+    ip_client = requests.get('https://api.ipify.org').text
+
+    print(Fore.LIGHTMAGENTA_EX+'search your location ...')
+    location_client = ipapi.location(ip_client)['country']
+    time.sleep(0.5)
+    print(Fore.LIGHTMAGENTA_EX+'Search Your city ...')
+    city = ipapi.location(ip_client)['region']
+    time.sleep(0.5)
+    print(Fore.LIGHTMAGENTA_EX+'Done')
+    time.sleep(1)
+    if os_name == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+    if location_client == 'IR':
+        print(Fore.RED+'[*] '+Fore.LIGHTCYAN_EX+'sorry you location' +
+              Fore.LIGHTRED_EX+' Iran '+Fore.LIGHTCYAN_EX+'please use vpn '.format(city))
+    else:
+        print(Fore.GREEN+'[*] '+Fore.LIGHTCYAN_EX+'so your location' +
+              Fore.LIGHTGREEN_EX+f' {city} '+Fore.LIGHTCYAN_EX+'is good you full acsess ')
+    print(city)
+
+
+find()
